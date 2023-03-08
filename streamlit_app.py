caff19f3332dd7005e05d8b3f47d6dab12dca5a4
DeekshaPremchandRao <127253585+DeekshaPremchandRao@users.noreply.github.com>
2023-03-08 14:50:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.header ('A header with _italics_ :blue[colors] and emojis :sunglasses:')
 streamlit.title('My Moms New Healthy Diner')
 streamlit.header('Breakfast Favourites')
@@ -7,4 +8,6 @@ streamlit.text('🥗 Kale, Spanish & Rocket Smothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
